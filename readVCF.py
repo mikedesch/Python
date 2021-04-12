3ad1583380334899c01e639ec8177c233b18b426
Mike Desch <mike.desch@gmail.com>
2021-04-12 17:06:20 -0500

    file updates 2021-04-12-17:06

---
+++ readVCF.py
@@ -0,0 +1,14 @@
+import vcfpy
+
+# Open file, this will read in the header
+reader = vcfpy.Reader.from_path('variants.vcf')
+
+# Build and print header
+header = reader.header.samples.names
+print('\t'.join(header))
+
+for record in reader:
+    if not record.is_snv():
+        continue
+    line = [call.data.get('GT') or './.' for call in record.calls]
+    print('\t'.join(map(str, line)))
